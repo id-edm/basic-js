de7b7ace4afd598e907235544a28adb6c57c7ac6
id-edm <ivandubochit@gmail.com>
2024-12-25 23:02:44 +0400

    feat: ready solutions createDreamTeam

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,10 +13,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+	if (!Array.isArray(members)) {
+		return false
+	}
+
+	let teamName = []
+
+	for (let i = 0; i < members.length; i += 1) {
+    const member = members[i]; 
+		if (typeof member === "string") {
+			teamName.push(member.trim()[0].toUpperCase())
+		}
+		teamName.sort()
+		fullNameTeam = teamName.join("")
+	}
+
+	return fullNameTeam;
 }
+
 
 module.exports = {
   createDreamTeam
